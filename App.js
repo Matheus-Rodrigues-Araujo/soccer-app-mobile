e62b20a2c0275fd4d3b8ff866569d23ddc1b9451
Matheus Rodrigues Araujo <matheus.rodgs.araujo13@gmail.com>
2023-05-28 23:30:47 -0300

    updated

--- App.js
+++ App.js
@@ -1,13 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { useEffect, useState } from 'react';
+import { Image } from 'react-native';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [data, setData] = useState([])
+
+  const getData = async()=>{
+    const url = 'https: api-football-v1.p.rapidapi.com/v3/leagues';
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'a44ca3f124mshf5256df877ee8a2p16ed0djsn312ab59cbb57',
+        'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+      setData(JSON.parse(result))
+        console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+    }
+
+
+    useEffect(()=>{
+      getData()
+    }, [])
+
+    return (
+      <>
+        <View style={styles.container}>
+          {/* <Text>Type of data: {data?.response && data?.response[0].league.name}</Text> */}
+          <StatusBar style="auto" />
+        </View>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app right now!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </>
+    );
+
+   
 }
 
 const styles = StyleSheet.create({
