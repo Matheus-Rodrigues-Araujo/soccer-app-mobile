b446dd6f0b65769d94e2e4eadb66e4237e92c591
Matheus Rodrigues Araujo <matheus.rodgs.araujo13@gmail.com>
2023-06-15 00:22:57 -0300

    Homepage has been updated with design changes

--- App.js
+++ App.js
@@ -1,66 +1,69 @@
 import { StyleSheet, Text, View, Button , ScrollView } from 'react-native';
 import { useEffect, useState } from 'react';
-import { Image } from 'react-native';
-
-
+import { TextInput } from 'react-native';
+import { Pressable } from 'react-native';
 
 export default function App() {
-  const [text, setText] = useState('')
-
-  const iconsList = {
-        thunder: <svg  xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#39ff2c" class="bi bi-lightning-charge-fill" viewBox="0 0 16 16">
-        <path d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09z"/>
-      </svg>
-    }
-
-
-    const xml = `
-      <svg  xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#39ff2c" class="bi bi-lightning-charge-fill" viewBox="0 0 16 16">
-        <path d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09z"/>
-      </svg>
-    `
-
+  const [text, setText] = useState('value')
+  const [value, onChangeText] = useState('');
 
     return(
       <View style={styles.container} >
-        <View
-        style={{
-          backgroundColor: 'black',
-          height: '10%',
-          padding: 10
-        }}
-        >
-          <Text style={{color: 'white', textAlign: 'center', fontWeight: 700, fontSize: 20, padding: 10}}>
-            Soccer{}News
+        <View style={{padding: 10,}}>
+          <Text style={{color: 'white', textAlign: 'center', fontWeight: 900, fontSize: 25, padding: 10}} >
+            Soccer News
           </Text>
         </View>
 
-        <View
-        style={{
-          height: '10',
-          padding:10,
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'center',
-          gap: 5,
-        }}
-        >
+        <View style={styles.navbar}>
+            <TextInput
+              editable
+              maxLength={30}
+              placeholder='Search...'
+              onChangeText={text => onChangeText(text)}
+              value={value}
+              style={styles.input}
+            />
 
-          <Button title='Leagues' style={styles.button} color='rgb(57, 255, 44)' onPress={()=> setText('Leagues page')} />
-          <Button title='Teams' style={styles.button} color='rgb(57, 255, 44)' onPress={()=> setText('Teams page')}/>
-          <Button title='Players' style={styles.button} color='rgb(57, 255, 44)' onPress={()=> setText('Players')}/>
-          <Button title='Matches'  style={styles.button} color='rgb(57, 255, 44)' onPress={()=> setText('Matches')}/>
+            <View style={styles.buttonsContainer} >
+              <Pressable
+                style={styles.button} 
+                backgroundColor='white'
+                onPress={()=> setText('Leagues page')}> 
+                <Text style={styles.buttonText}>Leagues</Text>
+              </Pressable>
+              
+              <Pressable 
+                style={styles.button} 
+                backgroundColor='white'
+                onPress={()=> setText('Teams page')}
+                
+                >
+                <Text style={styles.buttonText}>Teams</Text>
+              </Pressable>
+              
+              <Pressable 
+                style={styles.button} 
+                backgroundColor='white'
+                onPress={()=> setText('Players')}>
+                <Text style={styles.buttonText}>Players</Text>
+               </Pressable>
 
+              <Pressable
+                style={styles.button} 
+                backgroundColor='white'
+                onPress={()=> setText('Matches')}>
+                <Text style={styles.buttonText}>Matches</Text>
+              </Pressable> 
+            </View>
         </View>
 
         <View>
-            {text && text}
+            {text && value && <Text style={{color:'white'}} > {text + ' '+value} </Text>}
         </View>
-        
+
       </View>
     )
-
-   
 }
 
 const styles = StyleSheet.create({
@@ -69,10 +72,44 @@ const styles = StyleSheet.create({
     flex: 1,
     display: 'flex',
     flexDirection: 'column',
-    paddingTop: 30
+    paddingTop: 30,
+    backgroundColor: 'black',
+    padding:10,
+  },
+  navbar:{
+    padding:10,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    gap: 3,
+  },
+  buttonsContainer:{
+    padding:10,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 5,
   },
   button: {
     flex: 1,
-    height: '30px',
+    backgroundColor: 'rgb(57, 255, 44)',
+    borderRadius:3,
+    padding:5,
+  },
+  buttonText:{
+    textAlign: 'center',
+    textTransform:'uppercase',
+    fontWeight:800,
+    marginVertical: 'auto'
+  },
+  input: {
+    height: 40,
+    margin: 10,
+    borderWidth: 3,
+    padding: 10,
+    borderRadius:5,
+    borderColor: 'rgb(57, 255, 44)',
+    backgroundColor: 'white',
+    textDecorationLine:'none'
   }
 });
