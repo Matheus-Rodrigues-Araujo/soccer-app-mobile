89692d800cc299021e09a7a9f22cd9f842f377b5
Matheus Rodrigues Araujo <matheus.rodgs.araujo13@gmail.com>
2023-06-14 22:44:19 -0300

    updated

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button , ScrollView } from 'react-native';
 import { useEffect, useState } from 'react';
 import { Image } from 'react-native';
-import axios from 'axios';
+
+
+
 export default function App() {
-  // const [data, setData] = useState([])
+  const [text, setText] = useState('')
 
   const iconsList = {
         thunder: <svg  xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#39ff2c" class="bi bi-lightning-charge-fill" viewBox="0 0 16 16">
@@ -12,51 +13,48 @@ export default function App() {
       </svg>
     }
 
-  /* const getData = async()=>{
-  //   const options = {
-  //     method: 'GET',
-  //     url: 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0'
-  //   };
-    
-  //   try {
-  //     const response = await axios.request(options);
-  //     setData(response.data);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  //   }
 
+    const xml = `
+      <svg  xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#39ff2c" class="bi bi-lightning-charge-fill" viewBox="0 0 16 16">
+        <path d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09z"/>
+      </svg>
+    `
 
-  //   useEffect(()=>{
-  //     getData()
-   }, [])*/
 
     return(
       <View style={styles.container} >
         <View
         style={{
           backgroundColor: 'black',
-          height: '8%',
+          height: '10%',
           padding: 10
         }}
         >
-          <Text style={{color: 'white', textAlign: 'center', fontWeight: 700, fontSize: 20, padding: 10}}>Soccer
-          News</Text>
+          <Text style={{color: 'white', textAlign: 'center', fontWeight: 700, fontSize: 20, padding: 10}}>
+            Soccer{}News
+          </Text>
         </View>
 
         <View
         style={{
-          height: '90%',
-          padding:10
+          height: '10',
+          padding:10,
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          gap: 5,
         }}
         >
 
-          <Text style={{color: 'black', fontWeight: 700,textAlign: 'center', fontSize: 25}}>
-            Soccer information everyday!
-          </Text>
-          <Text style={{color: 'black', marginTop: 5}}>
-            Get to know all the statistics, news and curiosities of soccer around the world. With our website you will have in real time the most precious data of sports around the world!
-          </Text>
+          <Button title='Leagues' style={styles.button} color='rgb(57, 255, 44)' onPress={()=> setText('Leagues page')} />
+          <Button title='Teams' style={styles.button} color='rgb(57, 255, 44)' onPress={()=> setText('Teams page')}/>
+          <Button title='Players' style={styles.button} color='rgb(57, 255, 44)' onPress={()=> setText('Players')}/>
+          <Button title='Matches'  style={styles.button} color='rgb(57, 255, 44)' onPress={()=> setText('Matches')}/>
+
+        </View>
+
+        <View>
+            {text && text}
         </View>
         
       </View>
@@ -71,8 +69,10 @@ const styles = StyleSheet.create({
     flex: 1,
     display: 'flex',
     flexDirection: 'column',
-    // alignItems: 'center',
-    // justifyContent: 'flex-start',
-    paddingTop: 30,
+    paddingTop: 30
   },
+  button: {
+    flex: 1,
+    height: '30px',
+  }
 });
