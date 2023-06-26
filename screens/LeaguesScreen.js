d533714224ff995fd0f60c437db93b94945ca1a3
Matheus Rodrigues Araujo <matheus.rodgs.araujo13@gmail.com>
2023-06-26 00:21:08 -0300

    New: LeaguesScreen.js: handleSearch function created

--- screens/LeaguesScreen.js
+++ screens/LeaguesScreen.js
@@ -5,51 +5,22 @@ import LeagueCard from '../components/LeagueCard'
 
 const LeaguesScreen = () =>{
     const [leagues, setLeagues] = useState([])
-    // const [value, setValue] = useState("")
-    // const [filteredArray, setFilteredArray] = useState(array1);
     const [searchValue, setSearchValue] = useState('');
-   
-    
-   
-    //   const array1 = [
-    //     { name: 'John Doe', age: 25 },
-    //     { name: 'Jane Smith', age: 30 },
-    //     { name: 'Mark Johnson', age: 35 },
-    //     { name: 'Sara Anderson', age: 28 },
-    //   ];
-    
+
     
       const handleSearch = (text) => {
         setSearchValue(text);
-        const filteredData = leagues?.response.filter((item) =>
-          item.name.toLowerCase().includes(text.toLowerCase())
+        const filteredData = leagues.filter((item) =>
+          item.league['name'].toLowerCase().includes(text.toLowerCase())
         );
         setLeagues(filteredData);
       };
     
   
-    };
-    
-
-    function searchLeagues(){
-        if(leagues && leagues.response){
-            const matches = leagues.response.filter(item => {
-                item.name.includes('E')
-            }
-            )
-    
-            setLeagues(matches.response)
-        }
-        
-    }
-
-    useEffect(()=>{
-        searchLeagues()
-    }, [])
     // function search(arr, str){arr.filter(item => {for(let name in item){if(item.name.includes(str)){return console.log(item)}}return false})}
 
 
-    const getLeagues = async (newData=null) =>{
+    const getLeagues = async () =>{
 
         const options = {
             method: 'GET',
@@ -61,9 +32,10 @@ const LeaguesScreen = () =>{
         };
 
         try {
-            const response = await axios.request(options);
-            const {data} = response
-            setLeagues(data)
+            const result = await axios.request(options)
+            const {response} = result.data
+            setLeagues(response)
+            
         } catch (error) {
             console.error(error);
         }
@@ -73,53 +45,36 @@ const LeaguesScreen = () =>{
         getLeagues()
     },[])
 
-    // function Render (){
-    //     const {response} = leagues[0]
-    //     return(
-    //         <View style={{padding: 10}} >
-    //         {
-    //         response && response.map(
-    //             (e, index)=>(<LeagueCard
-    //                 key={index}
-    //                 name={e.league.name}
-    //                 logo={e.league.logo}
-    //             />))
-    //         }
-    //         </View>
-    //     )
-    // }
-
-
-    if(leagues.response){
+   
+    if(leagues){
         return(
             <ScrollView style={{backgroundColor: '#0B0B0B'}} >
                 <Text style={{color: 'white', fontSize: 25, textAlign: 'center'}} >Find your favorite Leagues</Text>
                 <Text style={{color: 'gray', fontSize: 20, textAlign: 'center'}} >Choose a league to explore!</Text>
+                
                 <TextInput
                   editable
                   maxLength={30}
                   placeholder='Search...'
                   style={leagueScreenStyle.input}
-                  value={'E'}
-                  onChangeText = {searchLeagues}
+                  value={searchValue}
+                  onChangeText={handleSearch}
+                  
                   
                 />
                  <View style={{padding: 10}} >
-                    {leagues.response && leagues.response.map(
+                    {
+                    leagues && leagues.map(
                         (e, index)=>(<LeagueCard
                             key={index}
                             name={e.league.name}
                             logo={e.league.logo}
-                        />))}
+                        />))
+                    }
                 </View>
+           
             </ScrollView>
          )
-    }else{
-        return(
-        <View style={{flex: 1, backgroundColor: '#0B0B0B', alignItems: 'center', justifyContent: 'center'}}>
-            <Text style={{color: 'white'}} >Loading...</Text>
-           </View>
-        )
     }
 
 
